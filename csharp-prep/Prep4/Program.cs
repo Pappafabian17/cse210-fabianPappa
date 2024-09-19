38ff205418a0382a1dd35cba90d5bac54c81cc10
Fabian Pappa <fabian.pappa.ext@errepar.com>
2024-09-19 08:24:53 -0300

    prep 4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,37 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        /* List<int> numbers = new List<int>();
-        List<string> words;
-        words = new List<string>();
-
-        words.Add("phone");
-        words.Add("mouse");
-        words.Add("headphones");
-
-        Console.WriteLine(words.Count);
-
-        foreach (string word in words){
-            Console.WriteLine(word);
-        }
-
-        for(int i = 0; i < words.Count;i++){
-            Console.WriteLine(words[i]);
-        } */
-
         List<int> numberList = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = -1 ;
+        float totalSum = 0;
+        float listLength = 0;
+        float average = 0;
+        int largest = 0;
+        int smallest = int.MaxValue;
 
-
-            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-
-        int userNumber ;
 
         while(userNumber != 0 ){
-            Console.WriteLine("Enter number:")
-            
+            Console.Write("Enter number:");
+            string auxNumber = Console.ReadLine();
+            int number = int.Parse(auxNumber);
+            numberList.Add(number);
+            userNumber = number;
+            totalSum = totalSum + number;
+            listLength = listLength + 1;
+            if(largest < number){
+                largest = number;
+            }
+            if( smallest > number && number != 0 && number > 0){
+                smallest = number;
+            }
         }
+        average = totalSum / (listLength -1);
 
 
-
+        Console.WriteLine($"The sum is : {totalSum}");
+        Console.WriteLine($"The average is : {average}");
+        Console.WriteLine($"The largest number is : {largest}");
+        Console.WriteLine($"The smallest number is : {smallest}");
+        numberList.Sort();
+        numberList.Reverse();
+        foreach(int number in numberList){
+            Console.WriteLine(number);
+        }
     }
 }
