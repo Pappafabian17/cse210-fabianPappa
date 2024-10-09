3ac5a13abbea00e2ff8951b0efc8ab3d3f04da13
Fabian Pappa <fabian.pappa.ext@errepar.com>
2024-10-09 19:25:29 -0300

    adding the image

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //the image is on the directory
         List<Video> videos = new List<Video>();
 
         Video video1 = new Video("How to use Classes in C#","Fabian",750);
@@ -12,7 +13,7 @@ class Program
         video1.AddComment(new Comment("Drake","It's awesome I love to play the guitar and you love to code"));
         video1.AddComment(new Comment("Sam","it's so difficult!"));
 
-        Video video2 = new Video("How to use Classes in C#","Fabian",750);
+        Video video2 = new Video("How to use Classes in C# part - 2","Juan Carlos",630);
         video2.AddComment(new Comment("Nephi","this does not exist on my age!"));
         video2.AddComment(new Comment("James","How can i improve?"));
         video2.AddComment(new Comment("Jayen","I like it!"));
