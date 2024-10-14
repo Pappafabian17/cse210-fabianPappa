d0d9513df9906970bffd421c7f903c1ad7e52b91
Fabian Pappa <fabian.pappa.ext@errepar.com>
2024-10-14 19:58:52 -0300

    learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment asignment1 = new Assignment("Fabian Pappa","Web programming");
+        Console.WriteLine(asignment1.GetSummary());
+
+        MathAssignment homework1 = new MathAssignment("juan","fractions","7.3","9-19");
+        Console.WriteLine(homework1.GetSummary());
+        Console.WriteLine(homework1.GetHomeworkList());
+
+        WritingAssignment homework2 = new WritingAssignment("Python data science","Hernan Pappa","science history");
+        Console.WriteLine(homework2.GetSummary());
+        Console.WriteLine(homework2.GetWritingInformation());
     }
 }
