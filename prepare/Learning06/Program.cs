5c74fe8fc8a1d84da2f00c3d35c373b396090abc
Fabian Pappa <fabian.pappa.ext@errepar.com>
2024-10-24 19:49:00 -0300

    learning polimorphism

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("Blue", 5);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("Green", 3,4);
+        shapes.Add( rectangle1 );
+
+        Circle circle1 = new Circle("Yellow", 54);
+        shapes.Add( circle1 );
+
+        foreach( Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($" The {color} shape has an area of {area}!");
+            Console.WriteLine();
+        }
     }
 }
